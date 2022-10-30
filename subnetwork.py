e47cbef7f467f9465d2765edd81a4c59af9cc606
leeuw <mohamedessabir20@gmail.com>
2022-10-30 21:43:22 +0100

    adding the export feature and finding subnet for ip in A and B classes

--- subnetwork.py
+++ subnetwork.py
@@ -1,4 +1,10 @@
+## Author: Leeuw
+## 10/30/22
+## find in at: 
+
 import argparse
+from tabulate import tabulate
+import csv
 
 # Initialize parser
 parser = argparse.ArgumentParser(prog="SubNets")
@@ -7,10 +13,10 @@ parser = argparse.ArgumentParser(prog="SubNets")
 parser.add_argument('-v', '--version', action="version", version="%(prog)s 0.1", help = "Show current version")
 parser.add_argument('-ip', nargs='?', const='192.168.0.0', default='192.168.0.0', help = "IP4 Address of a Network")
 parser.add_argument('-bit', '--BitsToBorrow', nargs='?', const='3', default='3', help= 'How many bits to borrow from hosts part in IP')
+parser.add_argument('--export', nargs='?', const='SubNet', help= 'Export to a csv file, default name of the file is SubNet')
 
 # Read arguments from command line
 args = parser.parse_args()
-
 
 def ValidIP(ip):
     '''Return True if ip is a valid IP4 address, False in the other case'''
@@ -113,29 +119,104 @@ def SubNetorks(ip, n):
     f1, f2, f3, f4 = ip.split(".")
 
     if DetectIPClass(ip) == 'A':
-        pass
+        f2 = int(f2)
+        f3 = int(f3)
+        f4 = int(f4)
+        for i in range(numSubNetworks(n)):
+            if int(n) < 8:
+                min = f2
+                dicData["id"] = i + 1
+                dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}'
+                f2 += int(((numHosts(ip, n) + 2) * 2**(-16)))
+                max = f2 - 2
+                dicData["Usable Host IP Range"] = f"[{f1}.{min}.0.1 - {f1}.{max}.255.254]"
+                dicData["IP Broadcast"] = f"{f1}.{max + 1}.255.255"
+            elif int(n) < 16:
+                if f3 == 256 :
+                    f2 += 1
+                    f3 = 0
+                min = f3
+                dicData["id"] = i + 1
+                dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}/{8 + int(n)}'
+                f3 += int(((numHosts(ip, n) + 2) * 2**(-8)))
+                max = f3 - 1
+                dicData["Usable Host IP Range"] = f"[{f1}.{f2}.{min}.1 - {f1}.{f2}.{max}.254]"
+                dicData["IP Broadcast"] = f"{f1}.{f2}.{max}.255"
+            else:
+                if f4 == 256 :
+                    f3 += 1
+                    f4 = 0
+                if f3 == 256 :
+                    f2 += 1
+                    f3 = 0
+                min = f4 + 1
+                dicData["id"] = i + 1
+                dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}'
+                f4 += (numHosts(ip, n) + 2)
+                max = f4 - 2
+                dicData["Usable Host IP Range"] = f"[{f1}.{f2}.{f3}.{min} - {f1}.{f2}.{f3}.{max}]"
+                dicData["IP Broadcast"] = f"{f1}.{f2}.{f3}.{max + 1}"
+            arr.append(dicData)
+            dicData = {}
+        return arr
     elif DetectIPClass(ip) == 'B':
-        pass
+        f3 = int(f3)
+        f4 = int(f4)
+        for i in range(numSubNetworks(n)):
+            if int(n) >= 8:
+                if f4 == 256 :
+                    f3 += 1
+                    f4 = 0
+                min = f4 + 1
+                dicData["id"] = i + 1
+                dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}'
+                f4 += (numHosts(ip, n) + 2)
+                max = f4 - 2
+                dicData["Usable Host IP Range"] = f"[{f1}.{f2}.{f3}.{min} - {f1}.{f2}.{f3}.{max}]"
+                dicData["IP Broadcast"] = f"{f1}.{f2}.{f3}.{max + 1}"
+            else:
+                min = f3
+                dicData["id"] = i + 1
+                dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}'
+                f3 += int(((numHosts(ip, n) + 2) * 2**(-8)))
+                max = f3 - 2
+                dicData["Usable Host IP Range"] = f"[{f1}.{f2}.{min}.1 - {f1}.{f2}.{max}.254]"
+                dicData["IP Broadcast"] = f"{f1}.{f2}.{max + 1}.255"
+            arr.append(dicData)
+            dicData = {}
+        return arr
     elif DetectIPClass(ip) == 'C':
-        f4 = int(f4) # 0 - 30 // 33 - 
+        f4 = int(f4) 
         for i in range(numSubNetworks(n)):
             min = f4 + 1
             dicData["id"] = i + 1
             dicData["Network IP"] = f'{f1}.{f2}.{f3}.{f4}'
             f4 += (numHosts(ip, n) + 2)
             max = f4 - 2
-            dicData["IP interval"] = f"[{min} - {max}]"
+            dicData["IP interval"] = f"[{f1}.{f2}.{f3}.{min} - {f1}.{f2}.{f3}.{max}]"
             dicData["IP Broadcast"] = f"{f1}.{f2}.{f3}.{max + 1}"
             arr.append(dicData)
             dicData = {}
         return arr
     return -1
 
-def DisplayTable():
+def DisplayTable(iterable):
     '''Display a Table that shows all the SubNetworks exist'''
+    if(iterable == -1):
+        print("Error!")
+        return -1
+    print(tabulate(iterable, headers="keys", tablefmt="orgtbl"))
+
 
 try:
-    print(SubNetorks(args.ip, args.BitsToBorrow))
-
+    if args.export:
+        with open(f"{args.export}.csv", 'w+', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'Network IP', 'Usable Host IP Range', 'IP Broadcast'])
+            writer.writeheader()
+            for dic in SubNetorks(args.ip, args.BitsToBorrow):
+                writer.writerow(dic)
+    else:
+        DisplayTable(SubNetorks(args.ip, args.BitsToBorrow))
+    
 except argparse.ArgumentError as err:
-    print("err : ", err)
+    print(err)
